59230ae9024dd8a69c15efde502020b273392aa4
Pujaar <adityamazter@gmail.com>
2025-10-26 22:54:32 +0700

    change login selector

--- cypress/e2e/User-can-login.cy.js
+++ cypress/e2e/User-can-login.cy.js
@@ -2,18 +2,18 @@ describe('User can login to system', () => {
   //positive
   it('User can login with valid email and password', () => {
     cy.visit('/login')
-    cy.get('div:nth-child(2) > .form-control').type('user@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password')
-    cy.get('.btn').click()
+    cy.get('[data-id="email"]').type('user@gmail.com')
+    cy.get('[data-id="password"]').type('password')
+    cy.get('[data-id="submit"]').click()
     cy.url().should('include', '/dashboard')
     cy.get('.d-sm-none.d-lg-inline-block').should('contain', 'Hi, user')
   })
 
   it('Superadmin can login with valid email and password', () => {
     cy.visit('/login')
-    cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password')
-    cy.get('.btn').click()
+    cy.get('[data-id="email"]').type('superadmin@gmail.com')
+    cy.get('[data-id="password"]').type('password')
+    cy.get('[data-id="submit"]').click()
     cy.url().should('include', '/dashboard')
     cy.get('.d-sm-none.d-lg-inline-block').should('contain', 'Hi, SuperAdmin')
   })
@@ -21,46 +21,46 @@ describe('User can login to system', () => {
   //negative
   it('User cannot login with invalid email and password', () => {
     cy.visit('/login')
-    cy.get('div:nth-child(2) > .form-control').type('superadmi@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password1')
-    cy.get('.btn').click()
+    cy.get('[data-id="email"]').type('superadmi@gmail.com')
+    cy.get('[data-id="password"]').type('password1')
+    cy.get('[data-id="submit"]').click()
     cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
   });
   
   it('User cannot login with invalid email', () => {
     cy.visit('/login')
-    cy.get('div:nth-child(2) > .form-control').type('superadmi@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password')
-    cy.get('.btn').click()
+    cy.get('[data-id="email"]').type('superadmi@gmail.com')
+    cy.get('[data-id="password"]').type('password')
+    cy.get('[data-id="submit"]').click()
     cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
   });
 
   it('User cannot login with invalid password', () => {
     cy.visit('/login')
-    cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password1')
-    cy.get('.btn').click()
+    cy.get('[data-id="email"]').type('superadmin@gmail.com')
+    cy.get('[data-id="password"]').type('password1')
+    cy.get('[data-id="submit"]').click()
     cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
   });
 
    it('User cannot login with blank email and password', () => {
     cy.visit('/login')
-    cy.get('.btn').click();
+    cy.get('[data-id="submit"]').click();
     cy.get('.invalid-feedback').eq(0).should('have.text', 'The email field is required.')
     cy.get('.invalid-feedback').eq(1).should('have.text', 'The password field is required.')
   });
 
   it('User cannot login with blank email', () => {
      cy.visit('/login')
-     cy.get('div:nth-child(3) > .form-control').type('password')
-     cy.get('.btn').click()
+     cy.get('[data-id="password"]').type('password')
+     cy.get('[data-id="submit"]').click()
      cy.get('.invalid-feedback').eq(0).should('have.text', 'The email field is required.')
   });
   
   it('User cannot login with blank password', () => {
      cy.visit('/login')
-     cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
-     cy.get('.btn').click()
+     cy.get('[data-id="email"]').type('superadmin@gmail.com')
+     cy.get('[data-id="submit"]').click()
      cy.get('.invalid-feedback').eq(0).should('have.text', 'The password field is required.')
   });
   
