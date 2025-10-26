f539aa352064e6a92c8529a696a7b7ec9102bdd2
Pujaar <adityamazter@gmail.com>
2025-10-26 22:15:07 +0700

    add more login test case

--- cypress/e2e/User-can-login.cy.js
+++ cypress/e2e/User-can-login.cy.js
@@ -4,7 +4,7 @@ describe('User can login to system', () => {
     cy.visit('http://127.0.0.1:8000/')
     cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
     cy.get('div:nth-child(3) > .form-control').type('password')
-    cy.get('.btn').click();
+    cy.get('.btn').click()
     cy.get('.d-sm-none.d-lg-inline-block').should('contain', 'Hi, SuperAdmin')
   })
 
@@ -13,24 +13,37 @@ describe('User can login to system', () => {
     cy.visit('http://127.0.0.1:8000/')
     cy.get('div:nth-child(2) > .form-control').type('superadmi@gmail.com')
     cy.get('div:nth-child(3) > .form-control').type('password')
-    cy.get('.btn').click();
+    cy.get('.btn').click()
     cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
   });
 
-  it.only('User cannot login with invalid password', () => {
+  it('User cannot login with invalid password', () => {
     cy.visit('http://127.0.0.1:8000/')
     cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
     cy.get('div:nth-child(3) > .form-control').type('password1')
-    cy.get('.btn').click();
+    cy.get('.btn').click()
     cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
   });
 
-   it.only('User cannot login with blank username and password', () => {
+   it('User cannot login with blank email and password', () => {
     cy.visit('http://127.0.0.1:8000/')
-    cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
-    cy.get('div:nth-child(3) > .form-control').type('password1')
     cy.get('.btn').click();
-    cy.get('.invalid-feedback').should('have.text', 'These credentials do not match our records.')
+    cy.get('.invalid-feedback').eq(0).should('have.text', 'The email field is required.')
+    cy.get('.invalid-feedback').eq(1).should('have.text', 'The password field is required.')
   });
 
+  it('User cannot login with blank email', () => {
+     cy.visit('http://127.0.0.1:8000/')
+     cy.get('div:nth-child(3) > .form-control').type('password')
+     cy.get('.btn').click()
+     cy.get('.invalid-feedback').eq(0).should('have.text', 'The email field is required.')
+  });
+  
+  it('User cannot login with blank password', () => {
+     cy.visit('http://127.0.0.1:8000/')
+     cy.get('div:nth-child(2) > .form-control').type('superadmin@gmail.com')
+     cy.get('.btn').click()
+     cy.get('.invalid-feedback').eq(0).should('have.text', 'The password field is required.')
+  });
+  
 })
